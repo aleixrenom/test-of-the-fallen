8148728740f966c3343bb5ca760760f74bad6f37
Aleix Renom <aleixrenom@gmail.com>
2021-02-19 19:34:07 +0200

    make it better

---
+++ index.js
@@ -0,0 +1,19 @@
+// Invitation link: https://discord.com/oauth2/authorize?client_id=812291101934616587&scope=bot
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.on('message', message => {
+	if (message.content === process.env.PREFIX + 'ping') {
+		// send back "Pong." to the channel the message was sent in
+		message.channel.send('Pong.');
+	}	
+});
+
+client.login(process.env.TOKEN);
