5d1b9f58765948e6f8e36ab384763358dc8a0649
Aleix Renom <aleixrenom@gmail.com>
2021-07-27 18:53:10 +0300

    asdf

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -7,9 +7,9 @@ module.exports = {
 	cooldown: 1,
 	async execute(message) {
 		const weathermanId = await qh.getId("role", "admin");
-		message.channel.send("```json\n" + JSON.stringify(message.member._roles, null, 2) + "\n```");
+		message.channel.send("```json\n" + message.member._roles.includes(weathermanId) + "\n```");
 		console.log(message.member._roles);
-		console.log("Has: " + message.member._roles.has(weathermanId));
+		console.log("Includes: " + message.member._roles.includes(weathermanId));
 		if (!message.member.roles.cache.has(weathermanId)) {
 			// message.channel.send("You do not have the admin role.");
 			return;
