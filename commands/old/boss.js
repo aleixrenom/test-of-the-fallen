b04906bdf7935125d998e0c2740c66807549610a
Aleix Renom <aleixrenom@gmail.com>
2021-07-28 18:53:00 +0300

    asdf

--- commands/old/boss.js
+++ commands/old/boss.js
@@ -11,27 +11,41 @@ module.exports = {
 			return;
 		}
 
-		if (args.length <= 0) {
-			message.channel.send("Usage: `+boss [change in hp]`");
-		} else {
-			try {
-				const data = await qh.getStorage("boss_hp");
-				console.log(data[0]);
-				console.log("Field: " + data[0].field_a + " of type " + typeof data[0].field_a);
-				const oldHp = parseInt(data[0].field_a);
-				console.log("Old hp: " + oldHp + " of type " + typeof oldHp);
-				const newHp = oldHp + parseInt(args[1]);
-				console.log("New hp: " + newHp + " of type " + typeof newHp);
-				await qh.setStorage("boss_hp", newHp.toString(), null, null);
-				console.log(message.member.user.username + " changed the boss HP from " + oldHp + " to " + newHp);
-				cf.client.channels.cache.get(await qh.getId("channel", "boss_hp")).send(
-					message.member.user.username + " changed the boss HP from " + oldHp + " to " + newHp
-				);
-				message.channel.send("Boss HP updated successfuly.");
-			} catch(err) {
-				console.error("Error modifying the boss hp: " + err);
-				message.channel.send("Error modifying the boss hp: " + err);
-			}
+		switch(args[1]) {
+			case("heal"):
+				if (isNaN(parseInt(args[2]))) {
+					message.channel.send("Given value is not a number.");
+					return;
+				}
+				if (args.length <= 1) {
+					message.channel.send("Specify an amount of HP to heal.");
+					return;
+				}
+
+				try {
+					const data = await qh.getStorage("boss_hp");
+					console.log(data[0]);
+					console.log("Field: " + data[0].field_a + " of type " + typeof data[0].field_a);
+					const oldHp = parseInt(data[0].field_a);
+					console.log("Old hp: " + oldHp + " of type " + typeof oldHp);
+					const newHp = oldHp + parseInt(args[2]);
+					console.log("New hp: " + newHp + " of type " + typeof newHp);
+					await qh.setStorage("boss_hp", newHp.toString(), null, null);
+					console.log(message.member.user.username + " changed the boss HP from " + oldHp + " to " + newHp);
+					cf.client.channels.cache.get(await qh.getId("channel", "boss_hp")).send(
+						message.member.user.username + " changed the boss HP from " + oldHp + " to " + newHp
+					);
+					message.channel.send("Boss HP updated successfuly.");
+				} catch(err) {
+					console.error("Error modifying the boss hp: " + err);
+					message.channel.send("Error modifying the boss hp: " + err);
+				}
+			break;
+			case("dmg" || "damage"):
+			break;
+			default:
+				message.channel.send("Usage:\n+boss dmg [damage taken]\n+boss damage [damage taken]\n+boss heal [health recovered]")
+			break;
 		}
 	},
 };
