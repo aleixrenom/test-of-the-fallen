7b4e2bcdb053246df03594ab4c6a69cea0ee9701
Aleix Renom <aleixrenom@gmail.com>
2021-12-17 19:06:59 +0200

    asdf

--- commands/administration/simulate.js
+++ commands/administration/simulate.js
@@ -13,8 +13,8 @@ module.exports = {
 		}
 
 		/**
-		 * 
-		 * @param {Int} players 
+		 * Generates the said amount of randomised attacks.
+		 * @param {Int} players Number of players attacking
 		 * @returns {Array} Array of rolls between 1 and 3 equal to players
 		 */
 		const generateAttacks = (players) => {
@@ -27,6 +27,11 @@ module.exports = {
 			return attacksArray;
 		}
 
+		/**
+		 * Checks which attack damages your roll.
+		 * @param {Int} attack The rolled attack
+		 * @returns {Int} The roll that would damage the rolled attack
+		 */
 		const getDamagingAttack = (attack) => {
 			switch (attack) {
 				case 1:
@@ -44,7 +49,11 @@ module.exports = {
 			}
 		}
 		
-		// returns true if players win
+		/**
+		 * Simulates one combat against Iku.
+		 * @param {Int} ikuHp HP that Iku has
+		 * @returns {Boolean} returns true if players win, false if Iku wins
+		 */
 		const simulateCombat = (ikuHp) => {
 			let combatFinished = false;
 			let enemyHp = ikuHp;
@@ -72,6 +81,29 @@ module.exports = {
 			return false;
 		}
 
-		message.channel.send(simulateCombat(15) ? "Players win" : "Iku wins").then().catch(e => console.error(e));
+		// If the arguments are correct...
+		if (args[0] && args[1] && !isNaN(args[0]) && !isNaN(args[1])) {
+			let playerVictories = 0;
+			let ikuVictories = 0;
+
+			for (let i = 0; i < args[0]; i++) {
+				if (simulateCombat(args[1])) {
+					playerVictories += 1;
+				} else {
+					ikuVictories += 1;
+				}
+			}
+
+			const returnString = `
+			I ran ${args[0]} simulations setting Iku-turso's HP to ${args[1]}.
+			Players won ${playerVictories} times.
+			Iku-turso won ${ikuVictories} times.
+			`;
+
+			message.channel.send(returnString).then().catch(e => console.error(e));
+		} else {
+			// If the arguments are not correct...
+			message.channel.send("Usage is `+simulate [number of simulations] [Iku's HP]`").then().catch(e => console.error(e));
+		}
 	},
 };
