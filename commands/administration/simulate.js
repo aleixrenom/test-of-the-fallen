b2cb17707177b379fea68667ae144c44d15ce72c
Aleix Renom <aleixrenom@gmail.com>
2021-12-17 17:52:32 +0200

    asdf

--- commands/administration/simulate.js
+++ commands/administration/simulate.js
@@ -5,7 +5,23 @@ module.exports = {
 	description: '',
 	cooldown: 1,
 	async execute(message, args) {
-		// message.channel.send(message.channel.parent.children.size).then().catch(err => console.error(err));
+		if (!message.member._roles.includes(String(await qh.getId("role", "admin")))) {
+			message.channel.send("This command is only avaliable to Admins.").then().catch(e => console.error(e));
+			return;
+		}
+
 		message.channel.send('Received.').then().catch(e => console.error(e));
+
+		const generateAttacks = (players) => {
+			let attacksArray = [];
+
+			for (let i = 0; i < players; i++) {
+				attacksArray.push(cf.rnd(1,3));
+			}
+
+			return attacksArray.toString();
+		}
+
+		message.channel.send(generateAttacks(7)).then().catch(e => console.error(e));
 	},
 };
